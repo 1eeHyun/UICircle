7eb98e29abaf80467b7e1fc39175b5d31118a3b0
1eeHyun <dlee2006@uic.edu>
2025-10-09 21:59:03 -0500

    chore: initialize Spring Boot backend structure

---
+++ backend/src/main/java/edu/uic/marketplace/UicMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package edu.uic.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UicMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UicMarketplaceApplication.class, args);
+	}
+
+}
